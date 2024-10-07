d4f890657ae9be297e77a56e1d1221df9d2e84fa
den4eg <Khristoliubov1986@gmail.com>
2024-10-07 11:31:24 +0300

    fix critical errors 0.05

--- client/src/redux/slices/authSLice.js
+++ client/src/redux/slices/authSLice.js
@@ -1,30 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    isLoggedIn: true,
-    user: null,
-    error: null,
-};
+// const initialState = {
+//     isLoggedIn: true,
+//     user: null,
+//     error: null,
+// };
 
-const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        loginSuccess: (state, action) => {
-            state.isLoggedIn = true;
-            state.user = action.payload;
-            state.error = null;
-        },
-        loginFailure: (state, action) => {
-            state.error = action.payload; // Set error message
-        },
-        logout: (state) => {
-            state.isLoggedIn = false;
-            state.user = null;
-            state.error = null;
-        },
-    },
-});
+// const authSlice = createSlice({
+//     name: 'auth',
+//     initialState,
+//     reducers: {
+//         loginSuccess: (state, action) => {
+//             state.isLoggedIn = true;
+//             state.user = action.payload;
+//             state.error = null;
+//         },
+//         loginFailure: (state, action) => {
+//             state.error = action.payload; // Set error message
+//         },
+//         logout: (state) => {
+//             state.isLoggedIn = false;
+//             state.user = null;
+//             state.error = null;
+//         },
+//     },
+// });
 
-export const { loginSuccess, loginFailure, logout } = authSlice.actions;
-export default authSlice.reducer;
+// export const { loginSuccess, loginFailure, logout } = authSlice.actions;
+// export default authSlice.reducer;
