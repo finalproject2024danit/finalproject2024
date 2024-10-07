3f652100503d582868f58f504086c5767a81d7a7
Olya Pavlova <oporoniuk@gmail.com>
2024-10-07 14:37:33 +0300

    Fix ?

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice.js";
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
+    reducer: {},
 });
 
 export default store;
