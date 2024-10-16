dff3341607eea447ff08a2118c50c1c4337c96ee
Дима Рябовол <rybovl.dima@gmail.com>
2024-10-16 23:07:14 +0300

    I made the user information page and also tried to make that when updating the user in the left side avatars and user name changed.

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,45 +1,58 @@
-// import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from "react-router-dom";
-import styles from './LeftSidebar.module.scss'
+import styles from './LeftSidebar.module.scss';
 import ExitIcon from "../../svg/Header/Exit";
 import PenIcon from "../../svg/Header/Pen";
 
-
 const LeftSidebar = () => {
-  return (
-   
+  const [avatar, setAvatar] = useState(localStorage.getItem('avatar') || 'default-avatar.png');
+  const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')) || {
+    firstName: '',
+    lastName: ''
+  });
 
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setAvatar(localStorage.getItem('avatar') || 'default-avatar.png');
+      setUserData(JSON.parse(localStorage.getItem('userData')) || { firstName: '', lastName: '' });
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  return (
     <aside className={styles.menu}>
-        <div>
-        <NavLink
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              to="/profile"            >
-              <div className={styles.iconWrapper}>
-              <img src={"https://res.cloudinary.com/dsr6kwzrr/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1728939216/photo_2024-10-14_23-52-52_zg5tc7.jpg"} alt="Post image" />
-              <PenIcon/>
-              </div>
-            </NavLink>
-        </div>
-        <div>
-          <form className={styles.menuForm} action="">
-            <input type="text" placeholder="SHTOTO-TAM" />
-            <button>Search</button>
-          </form>
-        </div>
-        <div className={styles.exit}>
-          <div className={styles.exitBlock}>
-            <img className={styles.exitBlockImg} src={"https://res.cloudinary.com/dsr6kwzrr/image/upload/w_70,c_fill,ar_1:1,g_auto,r_max,b_rgb:0D0F10/v1728939216/photo_2024-10-14_23-52-52_zg5tc7.jpg"} alt="Post image" />
-            <div className={styles.exitUser}>
-              <h4>Vasil</h4>
-              <h4>Chyhnar</h4>
-            </div>
+      <div>
+        <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to="/profile">
+          <div className={styles.iconWrapper}>
+            <img src={avatar} alt="User Avatar" />
+            <PenIcon />
           </div>
-          <div>
-            <a className={styles.exitBtn}><ExitIcon /></a>
+        </NavLink>
+      </div>
+      <div>
+        <form className={styles.menuForm} action="">
+          <input type="text" placeholder="Search" />
+          <button>Search</button>
+        </form>
+      </div>
+      <div className={styles.exit}>
+        <div className={styles.exitBlock}>
+          <img className={styles.exitBlockImg} src={avatar} alt="User Avatar" />
+          <div className={styles.exitUser}>
+            <h4>{userData.firstName}</h4>
+            <h4>{userData.lastName}</h4>
           </div>
         </div>
-      </aside>
-  
+        <div>
+          <a className={styles.exitBtn}><ExitIcon /></a>
+        </div>
+      </div>
+    </aside>
   );
 };
 
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import MainContent from "../../components/MainContent/MainContent.jsx";
 import styles from "./HomePage.module.scss";
