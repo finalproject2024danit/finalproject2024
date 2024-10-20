4f38b37d8500a0faa8c9c7bdf8f7edeb385e8831
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-20 19:12:33 +0300

    sql

--- src/main/resources/db/changelog/changeset/insert-data.sql
+++ src/main/resources/db/changelog/changeset/insert-data.sql
@@ -2,17 +2,17 @@ INSERT INTO users (
     first_name, last_name, email, password, gender, date_of_birth, avatar, phones, photo_data, created_date, last_modified_date
 ) VALUES
       (
-          'Alice', 'Johnson', 'alice.johnson@example.com', 'password123', 'Female', 631152000000,
+          'Alice', 'Johnson', 'alice.johnson@example.com', 'password123', 'FEMALE', 631152000000,
           'https://res.cloudinary.com/dsr6kwzrr/image/upload/v1728655770/runners_silhouettes_athletes_fitness_men_healthy_sunset_dusk-907233_ah4dpa.jpg', '12345',
           'https://res.cloudinary.com/dsr6kwzrr/image/upload/v1728655770/runners_silhouettes_athletes_fitness_men_healthy_sunset_dusk-907233_ah4dpa.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
       ),
       (
-          'Bob', 'Smith', 'bob.smith@example.com', 'securePass!', 'Male', 567993600000,
+          'Bob', 'Smith', 'bob.smith@example.com', 'securePass!', 'MALE', 567993600000,
           'https://res.cloudinary.com/dsr6kwzrr/image/upload/v1728655810/beautiful_girl_in_the_park_lying_on_the_leaves_autumn_portrait_romantic_park_feeling_in_love-1198265_pwzzdi.jpg', '12345',
           'https://res.cloudinary.com/dsr6kwzrr/image/upload/v1728655810/beautiful_girl_in_the_park_lying_on_the_leaves_autumn_portrait_romantic_park_feeling_in_love-1198265_pwzzdi.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
       ),
       (
-          'Charlie', 'Brown', 'charlie.brown@example.com', 'charliePass!', 'Non-Binary', 441763200000,
+          'Charlie', 'Brown', 'charlie.brown@example.com', 'charliePass!', 'NOT_SPECIFIED', 441763200000,
           'https://res.cloudinary.com/dsr6kwzrr/image/upload/v1728655814/fitness_jump_health_woman_girl_healthy_fit_sportive-1103572_mshkng.jpg', '12345',
           'User photo data for Charlie', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
       );
