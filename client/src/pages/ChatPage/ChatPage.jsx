b9d62197ce369ffdb41c7b2881c8b3e8d8b65d03
Olya Pavlova <oporoniuk@gmail.com>
2024-10-26 18:04:04 +0300

    star background

--- client/src/pages/ChatPage/ChatPage.jsx
+++ client/src/pages/ChatPage/ChatPage.jsx
@@ -5,9 +5,125 @@ import NewMessageForm from "./NewMessageForm/NewMessageForm";
 import MainContent from "../../components/MainContent/MainContent";
 import styles from "./ChatPage.module.scss";
 
+
 const ChatPage = () => {
   const dispatch = useDispatch();
   const { messages, selectedUser, loading } = useSelector((state) => state.chat);
+
+  useEffect(() => {
+    if (window.particlesJS) {
+        window.particlesJS("particles-js", {
+            particles: {
+                number: {
+                    value: 355,
+                    density: {
+                        enable: true,
+                        value_area: 789.15,
+                    },
+                },
+                color: {
+                    value: "#ffffff",
+                },
+                shape: {
+                    type: "circle",
+                    stroke: {
+                        width: 0,
+                        color: "#000000",
+                    },
+                    polygon: {
+                        nb_sides: 5,
+                    },
+                    image: {
+                        src: "img/github.svg",
+                        width: 100,
+                        height: 100,
+                    },
+                },
+                opacity: {
+                    value: 0.49,
+                    random: false,
+                    anim: {
+                        enable: true,
+                        speed: 0.25,
+                        opacity_min: 0,
+                        sync: false,
+                    },
+                },
+                size: {
+                    value: 2,
+                    random: true,
+                    anim: {
+                        enable: true,
+                        speed: 0.333,
+                        size_min: 0,
+                        sync: false,
+                    },
+                },
+                line_linked: {
+                    enable: false,
+                    distance: 150,
+                    color: "#ffffff",
+                    opacity: 0.4,
+                    width: 1,
+                },
+                move: {
+                    enable: true,
+                    speed: 0.1,
+                    direction: "none",
+                    random: true,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 1200,
+                    },
+                },
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: "bubble",
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    resize: true,
+                },
+                modes: {
+                    grab: {
+                        distance: 400,
+                        line_linked: {
+                            opacity: 1,
+                        },
+                    },
+                    bubble: {
+                        distance: 83.9,
+                        size: 1,
+                        duration: 3,
+                        opacity: 1,
+                        speed: 3,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                    push: {
+                        particles_nb: 4,
+                    },
+                    remove: {
+                        particles_nb: 2,
+                    },
+                },
+            },
+            retina_detect: true,
+        }); // Close the particlesJS function call here
+    }
+}, [dispatch]); // This is where the useEffect closing brace goes
 
   
   useEffect(() => {
@@ -24,10 +140,6 @@ const ChatPage = () => {
   const handleSendMessage = (newMessage) => {
     dispatch(sendMessage(newMessage)); // Dispatch the action to send a message
   };
-
-  const messagesList = [
-    // Array of message objects
-  ];
   
   
   
@@ -38,6 +150,9 @@ console.log('Filtered Messages:', filteredMessages);
 
   return (
     <MainContent title="Chat">
+<div id="particles-js">
+
+
     <div className={styles.container}>
         
         {/* Discussions Section */}
@@ -110,6 +225,7 @@ console.log('Filtered Messages:', filteredMessages);
             )}
           </section>
         
+    </div>
     </div>
     </MainContent>
   );
