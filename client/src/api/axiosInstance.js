226ea08dbb15ad3c1413fe17bf15ad5fd4eea988
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-29 01:13:37 +0200

    axios

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://134.209.246.21:9000/api/v1',
-  baseURL: 'http://localhost:9000/api/v1',
+  baseURL: 'http://134.209.246.21:9000/api/v1',
+  // baseURL: 'http://localhost:9000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   }
