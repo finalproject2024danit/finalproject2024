20b146d9b0fd635974076a16c9a35e14b64a1ff8
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-11-01 20:34:48 +0200

    Hobby

--- src/main/java/com/project/project/entities/friend/Friend.java
+++ src/main/java/com/project/project/entities/friend/Friend.java
@@ -1,7 +1,9 @@
 package com.project.project.entities.friend;
 
 import com.project.project.AbstractEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,9 +20,6 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Friend extends AbstractEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
 
     @Column(name = "user_from_id", nullable = false)
     Long userFromId;
