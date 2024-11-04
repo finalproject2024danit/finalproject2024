b86757c56e7217a228cb33ba41e15cd34ffb7a8b
Andrii Durytskyi <andy73594@gmail.com>
2024-11-04 13:05:02 +0200

    created groups

--- client/src/pages/GroupPage/GroupPage.jsx
+++ client/src/pages/GroupPage/GroupPage.jsx
@@ -1,97 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./GroupPage.module.scss";
 import MainContent from "../../components/MainContent/MainContent";
+import axiosInstance from "../../api/axiosInstance.js";
+
+const defaultAvatarGroup =
+  "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200,ar_16:9,c_fill,g_auto,e_sharpen/v1729669892/photo_2024-10-23_10-30-18_nmluce.jpg";
 
 const GroupPage = () => {
-  // Використання useState для зберігання активного індексу
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedGroup, setSelectedGroup] = useState(null);
 
-  // Функція для обробки кліку по зображенню
-  const handleImageClick = (index) => {
-    setActiveIndex(index);
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        const response = await axiosInstance.get(`/groups/filter`);
+        setGroups(response.data);
+      } catch (err) {
+        console.error("Помилка під час отримання даних групи:", err.message);
+        setError(`Помилка під час отримання даних групи: ${err.message}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchGroups();
+  }, []);
+
+  const fetchGroupDetails = async (groupId) => {
+    try {
+      const response = await axiosInstance.get(`/groups/${groupId}`);
+      setSelectedGroup(response.data);
+    } catch (err) {
+      console.error("Помилка під час отримання даних групи:", err.message);
+      setError(`Помилка під час отримання даних групи: ${err.message}`);
+    }
   };
+
+  if (loading) {
+    return (
+      <MainContent title="">
+        <p>Завантаження...</p>
+      </MainContent>
+    );
+  }
+
+  if (error) {
+    return (
+      <MainContent title="">
+        <p>{error}</p>
+      </MainContent>
+    );
+  }
 
   return (
     <MainContent title="">
-    <div className={styles.groupContainer}>
-      {/* Список зображень */}
-      <ul className={styles.groupHeader}>
-        {[
-          "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200/v1729506701/photo_2024-10-21_13-31-00_v386ym.jpg",
-          "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200/v1729507245/photo_2024-10-21_13-40-23_cxmnix.jpg",
-          "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200/v1729507453/photo_2024-10-21_13-43-46_jz3zuk.jpg",
-          "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200/v1729507548/photo_2024-10-21_13-45-21_fpr0jn.jpg",
-          "https://res.cloudinary.com/dsr6kwzrr/image/upload/w_200/v1729507680/photo_2024-10-21_13-47-27_xzqiiq.jpg"
-        ].map((imageSrc, index) => (
-          <li
-            key={index}
-            className={styles.groupList}
-            onClick={() => handleImageClick(index)} // Обробник кліку
-          >
-            <img src={imageSrc} alt={`Group ${index + 1}`} />
-          </li>
-        ))}
-      </ul>
-
-      {/* Відображення контенту відповідно до активного індексу */}
-      <ul className={styles.groupMain}>
-        {[
-          {
-            imgSrc: "https://res.cloudinary.com/dsr6kwzrr/image/upload/v1729506701/photo_2024-10-21_13-31-00_v386ym.jpg",
-            title: "First Group",
-            paragraph1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            paragraph2: "Phasellus auctor neque et arcu interdum, id venenatis metus aliquam.",
-            paragraph3: "Fusce convallis elit ut augue interdum, quis fringilla elit viverra."
-          },
-          {
-            imgSrc: "https://res.cloudinary.com/dsr6kwzrr/image/upload/v1729507245/photo_2024-10-21_13-40-23_cxmnix.jpg",
-            title: "Second Group",
-            paragraph1: "Donec quis lorem et felis vestibulum laoreet.",
-            paragraph2: "Vivamus facilisis magna ac ex molestie, nec luctus nunc tincidunt.",
-            paragraph3: "Aliquam erat volutpat. Vestibulum ante ipsum primis."
-          },
-          {
-            imgSrc: "https://res.cloudinary.com/dsr6kwzrr/image/upload/v1729507453/photo_2024-10-21_13-43-46_jz3zuk.jpg",
-            title: "Third Group",
-            paragraph1: "Aenean hendrerit neque nec justo faucibus, non lobortis lorem ullamcorper.",
-            paragraph2: "Cras consectetur, mi nec tempus interdum, dui libero hendrerit enim.",
-            paragraph3: "Suspendisse potenti. Nullam auctor sapien id lorem feugiat."
-          },
-          {
-            imgSrc: "https://res.cloudinary.com/dsr6kwzrr/image/upload/v1729507548/photo_2024-10-21_13-45-21_fpr0jn.jpg",
-            title: "Fourth Group",
-            paragraph1: "Curabitur dignissim lectus vitae ex bibendum, nec scelerisque metus sagittis.",
-            paragraph2: "Maecenas vehicula velit non urna fermentum volutpat.",
-            paragraph3: "Sed tincidunt lorem et libero mollis, vitae dapibus libero congue."
-          },
-          {
-            imgSrc: "https://res.cloudinary.com/dsr6kwzrr/image/upload/v1729507680/photo_2024-10-21_13-47-27_xzqiiq.jpg",
-            title: "Five Group",
-            paragraph1: "Curabitur dignissim lectus vitae ex bibendum, nec scelerisque metus sagittis.",
-            paragraph2: "Maecenas vehicula velit non urna fermentum volutpat.",
-            paragraph3: "Sed tincidunt lorem et libero mollis, vitae dapibus libero congue."
-          }
-        ].map((content, index) => (
-          <li
-            key={index}
-            className={
-              index === activeIndex ? styles.active : styles.hidden
-            } // Відображаємо лише активний елемент
-          >
-            <div className={styles.contentItem}>
-              <img src={content.imgSrc} alt={content.title} />
-              <h2>{content.title}</h2>
-              <p>{content.paragraph1}</p>
-              <p>{content.paragraph2}</p>
-              <p>{content.paragraph3}</p>
+      <div className={styles.groupContainer}>
+        <div className={styles.groupHeader}>
+          {groups.map((group) => (
+            <div
+              key={group.id}
+              onClick={() => fetchGroupDetails(group.id)}
+              className={styles.groupLink}
+            >
+              <h2>{group.name}</h2>
+              <img
+                src={group.avatar || defaultAvatarGroup}
+                alt={`${group.name} Avatar`}
+                className={styles.avatar}
+              />
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </div>
+        <div className={styles.groupRender}>
+          {selectedGroup && (
+            <>
+              <h3>{selectedGroup.name}</h3>
+              <div className={styles.groupPost}>
+                {selectedGroup.posts.map(post => (
+                  <div key={post.id} className={styles.post}>
+                    <p>{post.content}</p>
+                    <p>Created: {new Date(post.createdDate).toLocaleString()}</p>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </MainContent>
   );
- 
 };
 
 export default GroupPage;
@@ -99,3 +98,69 @@ export default GroupPage;
 
 
 
+// ______________________виводить всю інфу по групі___________________________
+
+// import React, { useEffect, useState } from "react";
+// import styles from "./GroupPage.module.scss";
+// import MainContent from "../../components/MainContent/MainContent";
+// import axiosInstance from "../../api/axiosInstance.js";
+
+// const GroupPage = ({ groupId }) => {
+//   const [group, setGroup] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchGroup = async () => {
+//       try {
+//         const response = await axiosInstance.get(`/groups/1`);
+//         console.log(response);
+//         setGroup(response.data);
+//       } catch (err) {
+//         console.error("Error fetching group data:", err.message);
+//         setError(`Error fetching group data: ${err.message}`);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchGroup();
+//   }, [groupId]);
+
+//   if (loading) {
+//     return <MainContent title=""><p>Loading...</p></MainContent>;
+//   }
+
+//   if (error) {
+//     return <MainContent title=""><p>{error}</p></MainContent>;
+//   }
+
+//   if (!group) {
+//     return <MainContent title=""><p>Group not found.</p></MainContent>;
+//   }
+
+//   return (
+//     <MainContent title={group.name}>
+//       <div className={styles.groupContainer}>
+//         <h1>{group.name}</h1>
+//         <p>{group.isOpen ? "This group is open." : "This group is closed."}</p>
+//         <h2>Posts</h2>
+//         {group.posts.length > 0 ? (
+//           group.posts.map(post => (
+//             <div key={post.id} className={styles.post}>
+//               <h3>Post by User {post.userId}</h3>
+//               <p>{post.content}</p>
+//               <p>Likes: {post.likes.length}</p>
+//               <p>Comments: {post.comments.length}</p>
+//               <p>Created on: {new Date(post.createdDate).toLocaleString()}</p>
+//             </div>
+//           ))
+//         ) : (
+//           <p>No posts available.</p>
+//         )}
+//       </div>
+//     </MainContent>
+//   );
+// };
+
+// export default GroupPage;
