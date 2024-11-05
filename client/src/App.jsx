7cb8dc2814338a5e6f4352eaa857941c28acefbc
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-11-05 19:44:49 +0200

    React-helmet-async

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import "./App.scss";
 // import "./variables.scss";
-
 import Header from "./components/Header";
 import AppRoutes from "./AppRoutes";
 import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
 import RightSidebar from "./components/RightSidebar/RightSidebar";
+import {Helmet} from "react-helmet-async";
 // import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return (
-    <div className="container">
-      <LeftSidebar />     
-      <div className="inner">
-        <Header />
-        <div>     
-          <div className="mainContainer">           
-            <div className="menuSocial">
-              <AppRoutes />
+    return (
+        <div className="container">
+            <Helmet>
+                <title>Galactic Connections</title>
+                <meta name='description' content='Galactic Connections - reach for the and connect!'/>
+                <meta name='keywords' content='space, earth, galaxy, connections'/>
+            </Helmet>
+            <LeftSidebar/>
+            <div className="inner">
+                <Header/>
+                <div>
+                    <div className="mainContainer">
+                        <div className="menuSocial">
+                            <AppRoutes/>
+                        </div>
+                        <RightSidebar/>
+                    </div>
+                </div>
             </div>
-            <RightSidebar />           
-          </div>         
         </div>
-      </div> 
-    </div>
-  );
+    );
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './reset.css';
 import './App.scss';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './redux/store.js';
+import {HelmetProvider} from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store}>
+        <BrowserRouter>
+            <HelmetProvider>
+                <App/>
+            </HelmetProvider>
+        </BrowserRouter>
+    </Provider>
 );
