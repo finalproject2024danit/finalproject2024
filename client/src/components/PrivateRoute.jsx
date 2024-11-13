b438058be805ade06b5617e3fb2461bbae47b226
Andrii Durytskyi <andy73594@gmail.com>
2024-11-13 12:12:33 +0200

    create loginPage start

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ children }) => {
-    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+// const PrivateRoute = ({ children }) => {
+//     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-    return isLoggedIn ? children : <Navigate to="/login" />;
-};
+//     return isLoggedIn ? children : <Navigate to="/login" />;
+// };
 
-PrivateRoute.propTypes = {
-    children: PropTypes.node.isRequired, // 'children' can be any renderable node
-};
+// PrivateRoute.propTypes = {
+//     children: PropTypes.node.isRequired, // 'children' can be any renderable node
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
