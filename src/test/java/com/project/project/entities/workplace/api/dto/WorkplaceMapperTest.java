1913c460b523075e6e07e03e0db9e6d14cb306c0
khatsubavladimir <send4vladimir@gmail.com>
2024-12-23 10:48:55 +0100

    workplace tests

---
+++ src/test/java/com/project/project/entities/workplace/api/dto/WorkplaceMapperTest.java
@@ -0,0 +1,20 @@
+package com.project.project.entities.workplace.api.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkplaceMapperTest {
+
+    @Test
+    void workplaceToResponseWorkplaceDto() {
+    }
+
+    @Test
+    void requestWorkplaceDtoToWorkplace() {
+    }
+
+    @Test
+    void requestUpdateWorkplaceDtoToWorkplace() {
+    }
+}
---
+++ src/test/java/com/project/project/entities/workplace/service/WorkplaceServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.project.project.entities.workplace.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkplaceServiceImplTest {
+
+    @Test
+    void addWorkplace() {
+    }
+
+    @Test
+    void getWorkplaceById() {
+    }
+
+    @Test
+    void getAllWorkplaces() {
+    }
+
+    @Test
+    void updateWorkplace() {
+    }
+
+    @Test
+    void getWorkplaceByName() {
+    }
+}
